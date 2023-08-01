645b4fe1479453ff1ee4811e87e47e1b5b2d3ff8
Olivier Pieters <me@olivierpieters.be>
2023-08-01 15:36:32 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
-# install fan control
+# install VNC, based on https://developer.nvidia.com/embedded/learn/tutorials/vnc-setup
+#   enable start at boot
+mkdir -p ~/.config/autostart
+cp /usr/share/applications/vino-server.desktop ~/.config/autostart/.
 
+# configure VNC
+gsettings set org.gnome.Vino prompt-enabled false
+gsettings set org.gnome.Vino require-encryption false
+
+# configure password
+gsettings set org.gnome.Vino authentication-methods "['vnc']"
+gsettings set org.gnome.Vino vnc-password $(echo -n 'gontrode2023'|base64)
+
+# install fan control
 sudo apt update
 sudo apt-get -y install git
 git clone https://github.com/Pyrestone/jetson-fan-ctl.git /tmp/jetson-fan-control
